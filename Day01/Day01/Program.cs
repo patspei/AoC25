4d5797aa9e3ca44d980b9574d6ca3ee887de523c
Patrick Speiser <202129@fhwn.ac.at>
2025-12-01 20:38:18 +0100

    add solution for day 01 part B

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -1,8 +1,8 @@
 ﻿
+// PART A:
 
 
-string input = File.ReadAllText("../../../input.txt");
-string[] lines = input.Split("\r\n");
+string[] lines = File.ReadAllLines("../../../input.txt");
 
 int zeroCounter = 0;
 int actualNumber = 50;
@@ -54,4 +54,68 @@ void Add(int number)
     }
 }
 
-Console.WriteLine(zeroCounter);
+Console.WriteLine($"Solution A: {zeroCounter}");
+
+
+
+// PART B:
+
+actualNumber = 50;
+zeroCounter = 0;
+
+foreach (var line in lines)
+{
+    if (line.StartsWith('L'))
+    {
+        int number = ExtractWithoutModulo(line);
+        GoLeft(number);
+    }
+    else
+    {
+        int number = ExtractWithoutModulo(line);
+        GoRight(number);
+    }
+}
+
+void GoRight(int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        DoOneStep(1);
+    }
+}
+
+void GoLeft(int number)
+{
+    for (int i = 0; i < number; i++)
+    {
+        DoOneStep(-1);
+    }
+}
+
+int ExtractWithoutModulo(string line)
+{
+    string numberedString = line.Remove(0, 1);
+    return int.Parse(numberedString);
+}
+
+void DoOneStep(int amount)
+{
+    actualNumber += amount;
+    if (actualNumber == 100)
+    {
+        actualNumber -= 100;
+    }
+
+    if (actualNumber == -1)
+    {
+        actualNumber += 100;
+    }
+
+    if (actualNumber == 0)
+    {
+        zeroCounter++;
+    }
+}
+
+Console.WriteLine($"Solution B: {zeroCounter}");
