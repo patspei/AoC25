f6ae4ecf61dec28f63bf9216c650779b631c1e27
Patrick Speiser <202129@fhwn.ac.at>
2025-12-02 13:22:13 +0100

    ad solution day 02 part B

--- Day02/Day02/Program.cs
+++ Day02/Day02/Program.cs
@@ -1,7 +1,9 @@
-﻿string input = File.ReadAllText("../../../input.txt");
+﻿
+string input = File.ReadAllText("../../../input.txt");
 string[] ranges = input.Split(',');
 
-long sumOfInvalidIds = 0;
+long sumOfInvalidIdsA = 0;
+long sumOfInvalidIdsB = 0;
 
 foreach (string range in ranges)
 {
@@ -11,11 +13,12 @@ foreach (string range in ranges)
 
     for (long i = from; i <= to; i++)
     {
-        CheckOneID(i.ToString());
+        CheckOneIDPartA(i.ToString());
+        CheckOneIDPartB(i.ToString());
     }
 }
 
-void CheckOneID(string id)
+void CheckOneIDPartA(string id)
 {
     if (id.Length % 2 != 0)
     {
@@ -30,7 +33,123 @@ void CheckOneID(string id)
         }
     }
 
-    sumOfInvalidIds += long.Parse(id);
+    sumOfInvalidIdsA += long.Parse(id);
 }
 
-Console.WriteLine(sumOfInvalidIds);
+void CheckOneIDPartB(string id)
+{
+    if (id.Length < 2)
+    {
+        return;
+    }
+
+    if (id.Length == 2)
+    {
+        if (CheckSingleDigitRepeat(id))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length == 3)
+    {
+        if (CheckSingleDigitRepeat(id))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length == 4)
+    {
+        if (CheckSingleDigitRepeat(id) || CheckMultipleDigitRepeat(id, 2))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length == 5)
+    {
+        if (CheckSingleDigitRepeat(id))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length == 6)
+    {
+        if (CheckSingleDigitRepeat(id) || CheckMultipleDigitRepeat(id, 2) || CheckMultipleDigitRepeat(id, 3))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length == 7)
+    {
+        if (CheckSingleDigitRepeat(id))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length == 8)
+    {
+        if (CheckSingleDigitRepeat(id) || CheckMultipleDigitRepeat(id, 2) || CheckMultipleDigitRepeat(id, 4))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length == 9)
+    {
+        if (CheckSingleDigitRepeat(id) || CheckMultipleDigitRepeat(id, 3))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length == 10)
+    {
+        if (CheckSingleDigitRepeat(id) || CheckMultipleDigitRepeat(id, 2) || CheckMultipleDigitRepeat(id, 5))
+        {
+            sumOfInvalidIdsB += long.Parse(id);
+        }
+    }
+
+    if (id.Length > 10)
+    {
+        Console.WriteLine("longer than 10");
+    }
+}
+
+bool CheckSingleDigitRepeat(string id)
+{
+    char first = id[0];
+
+    for (int i = 1; i < id.Length; i++)
+    {
+        if (first != id[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool CheckMultipleDigitRepeat(string id, int length)
+{
+    string first = id.Substring(0, length);
+
+    for (int i = length; i < id.Length; i+= length)
+    {
+        if (first != id.Substring(i, length))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+Console.WriteLine(sumOfInvalidIdsA);
+Console.WriteLine(sumOfInvalidIdsB);
